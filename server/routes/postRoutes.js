7520abeb4e210333805bdc01752e908c9610a710
gaurav4883 <gaurav.deol1600@gmail.com>
2023-02-17 01:52:10 +0545

    Backend completed

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+import { v2 as cloudinary } from 'cloudinary'
+import Post from '../mongodb/models/post.js'
+
+dotenv.config()
+
+const router = express.Router()
+
+
+
+
+export default router
