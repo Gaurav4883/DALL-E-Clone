b6a1d46dfc946adfd5233d0de67d66f5fb6ffb5f
gaurav4883 <gaurav.deol1600@gmail.com>
2023-02-17 15:18:12 +0545

    deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         const fetchPosts = async () => {
             setLoading(true)
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://dall-e-da2c.onrender.com/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
